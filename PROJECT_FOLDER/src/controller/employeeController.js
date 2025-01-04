a81b5925d3b1c4efe8fd6d5e0e66f96914c01c3e
rezarubik <muhammad.reza.pahlevi.y@gmail.com>
2025-01-05 02:04:17 +0700

    DONE for all CRUD Employee and CRUD Employee with its relation

--- PROJECT_FOLDER/src/controller/employeeController.js
+++ PROJECT_FOLDER/src/controller/employeeController.js
@@ -5,6 +5,7 @@ const {
   Education,
 } = require("../models");
 
+/* ----------------------- // start: Get All Employee ----------------------- */
 const getAllEmployees = async (req, res) => {
   try {
     const employees = await Employee.findAll({
@@ -23,7 +24,9 @@ const getAllEmployees = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+/* ----------------------- // end: Get All Employee ----------------------- */
 
+// start: Get One Employee (with all relation to profile, family & education)
 const getEmployeeById = async (req, res) => {
   const { id } = req.params;
   try {
@@ -37,28 +40,36 @@ const getEmployeeById = async (req, res) => {
     if (!employee) {
       return res.status(404).json({ message: "Employee not found" });
     }
-    res.json(employee);
+    res.json({
+      message: "Success get employe",
+      data: employee,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+// end: Get One Employee (with all relation to profile, family & education)
 
+/* -------------------------------------------------------------------------- */
+/*      // start: CRUD Employee with it's profile, family, and education      */
+/* -------------------------------------------------------------------------- */
+// todo: Create Employee with it's profile, family, and education
 const createEmployee = async (req, res) => {
   const { employeeData, profileData, familiesData, educationsData } = req.body;
 
   const t = await Employee.sequelize.transaction();
 
   try {
-    // Create Employee
+    // todo: Create Employee
     const employee = await Employee.create(employeeData, { transaction: t });
 
-    // Create Profile
+    // todo: Create Profile
     if (profileData) {
       profileData.employee_id = employee.id;
       await EmployeeProfile.create(profileData, { transaction: t });
     }
 
-    // Create Families
+    // todo: Create Families
     if (familiesData && familiesData.length) {
       const families = familiesData.map((family) => ({
         ...family,
@@ -67,7 +78,7 @@ const createEmployee = async (req, res) => {
       await EmployeeFamily.bulkCreate(families, { transaction: t });
     }
 
-    // Create Educations
+    // todo: Create Educations
     if (educationsData && educationsData.length) {
       const educations = educationsData.map((education) => ({
         ...education,
@@ -86,6 +97,7 @@ const createEmployee = async (req, res) => {
   }
 };
 
+// todo: Update Employee with it's profile, family, and education
 const updateEmployee = async (req, res) => {
   const { id } = req.params;
   const { employeeData, profileData, familiesData, educationsData } = req.body;
@@ -98,10 +110,10 @@ const updateEmployee = async (req, res) => {
       return res.status(404).json({ message: "Employee not found" });
     }
 
-    // Update Employee
+    // todo: Update Employee
     await employee.update(employeeData, { transaction: t });
 
-    // Update Profile
+    // todo: Update Profile
     if (profileData) {
       const profile = await EmployeeProfile.findOne({
         where: { employee_id: id },
@@ -114,7 +126,7 @@ const updateEmployee = async (req, res) => {
       }
     }
 
-    // Update Families
+    // todo: Update Families
     if (familiesData) {
       await EmployeeFamily.destroy({
         where: { employee_id: id },
@@ -127,7 +139,7 @@ const updateEmployee = async (req, res) => {
       await EmployeeFamily.bulkCreate(families, { transaction: t });
     }
 
-    // Update Educations
+    // todo: Update Educations
     if (educationsData) {
       await Education.destroy({ where: { employee_id: id }, transaction: t });
       const educations = educationsData.map((education) => ({
@@ -145,13 +157,14 @@ const updateEmployee = async (req, res) => {
   }
 };
 
+// todo: Delete Employee with it's profile, family, and education
 const deleteEmployee = async (req, res) => {
   const { id } = req.params;
 
   const t = await Employee.sequelize.transaction();
 
   try {
-    // Delete related data first
+    // todo: Delete related data first
     await EmployeeProfile.destroy({
       where: { employee_id: id },
       transaction: t,
@@ -162,7 +175,7 @@ const deleteEmployee = async (req, res) => {
     });
     await Education.destroy({ where: { employee_id: id }, transaction: t });
 
-    // Delete Employee
+    // todo: Delete Employee
     const employee = await Employee.destroy({ where: { id }, transaction: t });
     if (!employee) {
       return res.status(404).json({ message: "Employee not found" });
@@ -175,11 +188,108 @@ const deleteEmployee = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+/* -------------------------------------------------------------------------- */
+/*       // end: CRUD Employee with it's profile, family, and education       */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                      // start: CRUD in Employee Table                      */
+/* -------------------------------------------------------------------------- */
+const getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.findAll();
+    res.status(200).json({
+      message: "Success Get All Employee",
+      total: employees.length,
+      data: employees,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message + " test" });
+  }
+};
+
+const createOneEmployee = async (req, res) => {
+  try {
+    const { employeeData } = req.body;
+    const employee = await Employee.create(employeeData);
+    res.status(201).json({
+      message: "Success create employee",
+      data: employee,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateOneEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { employeeData } = req.body;
+    const [updated] = await Employee.update(employeeData, { where: { id } });
+    if (!updated)
+      return res.status(404).json({ message: "Employee not found" });
+    res.status(200).json({ message: "Employee updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteOneEmployee = async (req, res) => {
+  const t = await Employee.sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findByPk(id, {
+      include: [
+        { model: EmployeeProfile, as: "profile" },
+        { model: EmployeeFamily, as: "families" },
+        { model: Education, as: "educations" },
+      ],
+    });
+    if (!employee) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+    // console.log(
+    //   "employee",
+    //   employee.profile,
+    //   employee.families,
+    //   employee.educations
+    // );
+    if (
+      employee.profile !== null ||
+      (employee.families.length = 0) ||
+      (employee.educations.length = 0)
+    ) {
+      await t.rollback();
+      res.status(200).json({
+        message:
+          "Oops sorry, you can not delete this employe because still have relation",
+        data: employee,
+      });
+    } else {
+      const deleted = await Employee.destroy({ where: { id } });
+      if (!deleted) {
+        return res.status(404).json({ message: "Employee not found" });
+      }
+      await t.commit();
+      res.status(200).json({ message: "Employee deleted successfully" });
+    }
+  } catch (error) {
+    await t.rollback();
+    res.status(500).json({ message: error.message });
+  }
+};
+/* -------------------------------------------------------------------------- */
+/*                       // end: CRUD in Employee Table                       */
+/* -------------------------------------------------------------------------- */
 
 module.exports = {
-  getAllEmployees,
+  getAllEmployees, //note: With it's relation
   getEmployeeById,
   createEmployee,
   updateEmployee,
   deleteEmployee,
+  getEmployees, //note: Just employee
+  createOneEmployee,
+  updateOneEmployee,
+  deleteOneEmployee,
 };
--- PROJECT_FOLDER/src/routes/employeeRoutes.js
+++ PROJECT_FOLDER/src/routes/employeeRoutes.js
@@ -5,14 +5,24 @@ const {
   createEmployee,
   updateEmployee,
   deleteEmployee,
+  getEmployees,
+  createOneEmployee,
+  updateOneEmployee,
+  deleteOneEmployee,
 } = require("../controller/employeeController");
 
 const router = express.Router();
 
 router.get("/", getAllEmployees);
+
+router.get("/all", getEmployees); // note: Just get all employee from employee table
 router.get("/:id", getEmployeeById);
 router.post("/", createEmployee);
-router.put("/:id", updateEmployee);
-router.delete("/:id", deleteEmployee);
+router.put("/:id", updateEmployee); // note: Update Employee with it's relation
+router.patch("/:id", updateOneEmployee); // note: Update Employee without it's relation
+router.delete("/:id", deleteEmployee); // note: Delete Employee with it's relation
+// todo: CRUD one data in Employee Table
+router.post("/create-one", createOneEmployee);
+router.delete("/one/:id", deleteOneEmployee); // note: Delete Employee without it's relation
 
 module.exports = router;
